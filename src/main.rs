4fbbe164f8fdfcf7070f750ca228cbba82a8e92b
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2024-07-07 14:06:13 +0530

    Checkpoint commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::Parser;
+
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(Parser)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    // Building a grep clone
+    // let pattern = std::env::args().nth(1).expect("no pattern given");
+    // let path = std::env::args().nth(2).expect("no path given");
+    //
+    // let args = Cli {
+    //     pattern: pattern,
+    //     path: std::path::PathBuf::from(path),
+    // };
+    
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("Could not read file");
+
+    for line in content.lines() {
+        if line.contains(&args.pattern){
+            println!("{}", line);
+        }
+    }
+
+    // println!("Pattern: {:?}, path: {:?}", args.pattern, args.path)
+
 }
